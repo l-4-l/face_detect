95825d7e8b5e9e6e6612684d18878d04105ae145
l-4-l <lordakryl@gmail.com>
2017-11-06 17:45:53 +0100

    + eyes

--- src/face_detect.py
+++ src/face_detect.py
@@ -9,10 +9,12 @@ if len(sys.argv) > 1:
 else:
     imagePath = "abba.png"
 
-cascPath = "haarcascade_frontalface_default.xml"
+cascPath = "./haarcascades/haarcascade_frontalface_default.xml"
+cascEyePath = "./haarcascades/haarcascade_eye.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+eyeCascade = cv2.CascadeClassifier(cascEyePath)
 
 # Read the image
 image = cv2.imread(imagePath)
@@ -27,11 +29,24 @@ faces = faceCascade.detectMultiScale(
     #,flags = cv2.CV_HAAR_SCALE_IMAGE
 )
 
-print("Found {0} faces!".format(len(faces)))
+
+print("Found {0} faces".format(len(faces)))
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.line(image, )
+    eyes = eyeCascade.detectMultiScale(
+        cv2.cvtColor(image[y:(y+h), x:(x+w)], cv2.COLOR_BGR2GRAY),
+        scaleFactor=1.1,
+        minNeighbors=2,
+        minSize=(2, 2)
+        #,flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+    for (ex, ey, ew, eh) in eyes:
+        cv2.circle(image, (x+ex+ew/2, y+ey+eh/2), ew/2, (0,255,255))
+        #def circle(img, center, radius, color, thickness=None, lineType=None, shift=None)
+
+
+
 
 cv2.imwrite("abba.recog.png", image)
