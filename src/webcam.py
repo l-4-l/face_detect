87a3082f9936b9e068fa50eedcb49ec8ce315013
l-4-l <lordakryl@gmail.com>
2018-01-10 21:32:46 +0100

    playing with arrays - problem with dimensions. without TEst block it's working, but colors are wrong.

--- src/webcam.py
+++ src/webcam.py
@@ -10,9 +10,9 @@ import numpy as np
 from array import array
 
 
-def append_img_from_cam(img_arr, webcam):
-    img_copy = pygame.surfarray.array2d(webcam.get_image())
-    img_arr.append(img_copy)
+def append_img_from_cam(img_arr, webcam_l):
+    img_new = pygame.surfarray.array2d(webcam_l.get_image())
+    img_arr.append(img_new)
 
 
 pygame.camera.init()
@@ -20,54 +20,45 @@ cameras = pygame.camera.list_cameras()
 webcam = pygame.camera.Camera(cameras[0])
 webcam.start()
 
+
+# test
+def rgb_int2tuple(rgbint):
+    return (rgbint // 256 // 256 % 256, rgbint // 256 % 256, rgbint % 256)
+
+im_t = webcam.get_image()
+print im_t.get_bytesize()
+im_t_2d = pygame.surfarray.array2d(im_t).astype("uint32")
+im_t_2d[0][0] = 0x11000f
+print im_t_2d[0][0]
+print hex(im_t_2d[0][0])
+sf_t = pygame.surfarray.make_surface(im_t_2d)
+print sf_t.get_at((0, 0))
+webcam.stop()
+exit(0)
+
 # grab first frame
 img = []
 append_img_from_cam(img, webcam)
-image = webcam.get_image().copy()  #temp
-
-WIDTH = image.get_width()
-HEIGHT = image.get_height()
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+image_1 = webcam.get_image().copy()
+screen = pygame.display.set_mode((image_1.get_width(), image_1.get_height()))
 #pygame.display.set_caption("CamView")
 
 
 def avg_images(img):
-    images_count = len(img)
-
-    image_l = img[0].copy()  # result
-    #arr = pygame.surfarray.pixels3d(image_l)
-    #arr_float = arr.copy()
-    #for i in range(len(arr_float)):
-    #    for j in range(len(arr_float[i])):
-    #        for k in range(len(arr_float[i][j])):
-    #            arr_float[i][j][k] = float(arr_float[i][j][k]) / images_count
-
-    if images_count > 1:
-
-        for y in range(1, images_count):
-            arr_i = pygame.surfarray.pixels3d(img[y])
-
-            #arr_float = numpy.add(arr_float, arr_i)
-            #for i in range(len(arr)):
-            #    for j in range(len(arr_i[i])):
-            #        for k in range(len(arr_i[i][j])):
-            #            arr_float[i][j][k] += float(arr_i[i][j][k]) / images_count
-            #np.mean(list_of_arrays[t - N + 1:t + 1], axis=0)
-            del arr_i
-            img[y].unlock()
-
-    #del arr
-    #image_l.unlock()
-    return image_l
+    # print type(img[0])
+    rz = np.mean(img, axis=0, dtype='uint8')
+    # print type(rz)
+    return rz
 
 
-while True :
+image = img[0]
+image.get_
+while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             sys.exit()
-
-    screen.blit(image, (0, 0))
+    sf = pygame.surfarray.make_surface(image)
+    screen.blit(sf, (0, 0))
     pygame.display.flip()
     # grab next frame
     append_img_from_cam(img, webcam)
@@ -76,7 +67,5 @@ while True :
         img.pop(0)
 
     image = avg_images(img)
-
-
 
 #pygame.image.save(img, "image.jpg")
