8e069acc7ec247ed19f1b74495aa01d84d96ccd1
Andrey Bachmaga <andrey.bachmaga@rewe-digital.com>
2018-01-10 21:37:37 +0100

    Working!!111OneONEОдинОДИН

--- src/webcam.py
+++ src/webcam.py
@@ -11,7 +11,7 @@ from array import array
 
 
 def append_img_from_cam(img_arr, webcam_l):
-    img_new = pygame.surfarray.array2d(webcam_l.get_image())
+    img_new = pygame.surfarray.array3d(webcam_l.get_image())
     img_arr.append(img_new)
 
 
@@ -25,16 +25,15 @@ webcam.start()
 def rgb_int2tuple(rgbint):
     return (rgbint // 256 // 256 % 256, rgbint // 256 % 256, rgbint % 256)
 
-im_t = webcam.get_image()
-print im_t.get_bytesize()
-im_t_2d = pygame.surfarray.array2d(im_t).astype("uint32")
-im_t_2d[0][0] = 0x11000f
-print im_t_2d[0][0]
-print hex(im_t_2d[0][0])
-sf_t = pygame.surfarray.make_surface(im_t_2d)
-print sf_t.get_at((0, 0))
-webcam.stop()
-exit(0)
+#im_t = webcam.get_image()
+#print im_t.get_bytesize()
+#im_3d = pygame.surfarray.array3d(im_t)
+#print im_3d[0][0][0]
+#print hex(im_3d[0][0][0])
+#sf_t = pygame.surfarray.make_surface(im_3d)
+#print sf_t.get_at((0, 0))
+#webcam.stop()
+#exit(0)
 
 # grab first frame
 img = []
@@ -46,13 +45,12 @@ screen = pygame.display.set_mode((image_1.get_width(), image_1.get_height()))
 
 def avg_images(img):
     # print type(img[0])
-    rz = np.mean(img, axis=0, dtype='uint8')
+    rz = np.mean(img, axis=0)
     # print type(rz)
     return rz
 
 
 image = img[0]
-image.get_
 while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
